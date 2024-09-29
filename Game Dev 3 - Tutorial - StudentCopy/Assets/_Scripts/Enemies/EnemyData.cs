91e30386f73b27c9a689f11abbe9b3af5fc71189
Keslec3 <Videen999@gmail.com>
2024-09-29 17:20:30 +0100

    EnemyData scriptable object completed.

---
+++ Game Dev 3 - Tutorial - StudentCopy/Assets/_Scripts/Enemies/EnemyData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "EnemyData", menuName = "ScriptableObjects/EnemyData")]
+public class EnemyData : ScriptableObject
+{
+    public Sprite shipSprite;
+    public float shipSpeed;
+    public int shipHp;
+}
